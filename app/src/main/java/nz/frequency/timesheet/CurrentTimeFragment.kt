c38ee72f068b255f6cd6af8f0bad7eee4ffee1a4
jasonb <jasonb@frequency.nz>
2018-11-14 09:20:54 +1300

    App after the Maps for fragment within fragment was configured

--- app/src/main/java/nz/frequency/timesheet/CurrentTimeFragment.kt
+++ app/src/main/java/nz/frequency/timesheet/CurrentTimeFragment.kt
@@ -1,15 +1,57 @@
 package nz.frequency.timesheet
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class CurrentTimeFragment : Fragment() {
+class CurrentTimeFragment : Fragment(), OnMapReadyCallback {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.current_time_fragment_layout, container, false)
+    private lateinit var mMap: GoogleMap
+    private lateinit var mapFragment: SupportMapFragment
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var currentView =  inflater.inflate(R.layout.current_time_fragment_layout, container, false)
+        mapFragment = childFragmentManager.findFragmentById(R.id.map2) as SupportMapFragment
+
+        return currentView
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Auckland and move the camera
+        val auckland = LatLng(-36.8485, 174.7633)
+
+        val circleOptions = CircleOptions()
+        circleOptions.center(auckland)
+        circleOptions.radius(850.0)
+        circleOptions.fillColor(Color.LTGRAY)
+        circleOptions.strokeColor(Color.BLACK)
+        circleOptions.strokeWidth(3f)
+        mMap.addCircle(circleOptions)
+        mMap.addMarker(MarkerOptions().position(auckland).title("Marker in Auckland"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(auckland))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(mMap.cameraPosition.zoom + 10f))
+
+    }
+
+
 
     companion object {
         fun newInstance(): CurrentTimeFragment = CurrentTimeFragment()
